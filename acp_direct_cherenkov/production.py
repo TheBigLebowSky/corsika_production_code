8f755dcf4cf241c56858027d711d185caa04973a
sebastian <sebmuell@phys.ethz.ch>
2016-12-12 13:47:32 +0100

    add comments

--- acp_direct_cherenkov/production.py
+++ acp_direct_cherenkov/production.py
@@ -30,7 +30,6 @@ def eventio_extractor(eventio_path, output_path, eventio_extractor_path):
           
 
 def make_corsika_run(instruction):
-    
     with tempfile.TemporaryDirectory(prefix='dc_production_') as tmp_dir:
         eventio_path = os.path.join(tmp_dir, 'corsika_run.evtio')
 
@@ -57,6 +56,7 @@ def main():
     try:
         arguments = docopt.docopt(__doc__)
 
+        # Set uo the output directory structure
         config = {}
         config['path'] = {}
         config['path']['main'] = {}
@@ -67,36 +67,43 @@ def main():
             config['path']['main']['dir'],
             'input')
 
+        os.mkdir(config['path']['main']['dir'])
+        os.mkdir(config['path']['main']['input']['dir'])
+
+        # Copy the steering card for this production
         config['path']['main']['input']['steering'] = os.path.join(
             config['path']['main']['input']['dir'],
             'steering.json')
-
-        config['path']['main']['input']['corskia'] = os.path.join(
-            config['path']['main']['input']['dir'],
-            'corskia')
-
-        os.mkdir(config['path']['main']['dir'])
-        os.mkdir(config['path']['main']['input']['dir'])
 
         shutil.copy(
             arguments['--config'], 
             config['path']['main']['input']['steering'])
 
+        # Copy the actual CORSIKA directory used for this simulation
+        config['path']['main']['input']['corskia'] = os.path.join(
+            config['path']['main']['input']['dir'],
+            'corskia')
+
         shutil.copytree(
             dc.corsika_tools.corsika_directory(),
             config['path']['main']['input']['corskia'])
 
+        # Read in the production steering
         config['steering'] = dc.steering.read_steering(
             config['path']['main']['input']['steering'])
 
+        # Create the output directories for the different nuclei
         for nucleus in config['steering']['nuclei']:
             config['path']['main'][str(nucleus['PRMPAR'])] = os.path.join(
                 config['path']['main']['dir'],
                 dc.corsika_tools.PRMPAR_2_human_readable(nucleus['PRMPAR']))
             os.mkdir(config['path']['main'][str(nucleus['PRMPAR'])])
 
-        config['evtio_extractor'] = os.path.abspath(arguments['--evtio_extractor'])
+        # Set the mctracer eventio converter
+        config['evtio_extractor'] = os.path.abspath(
+            arguments['--evtio_extractor'])
 
+        # Make the instructions and start parallel production
         instructions = dc.instructions.make_instructions(config)
         result = list(scoop.futures.map(make_corsika_run, instructions)) 
 
